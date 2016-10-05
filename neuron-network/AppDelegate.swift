fe7bb59814eef2a48c31300db8fd63f8de66fed9
IshimotoKiko <ishimotokiko@ishimotokiko.local>
2016-10-05 12:19:28 +0900

    Initial Commit

---
+++ neuron-network/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  neuron-network
+//
+//  Created by IshimotoKiko on 2016/10/05.
+//  Copyright © 2016年 IshimotoKiko. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ neuron-network/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  neuron-network
+//
+//  Created by IshimotoKiko on 2016/10/05.
+//  Copyright © 2016年 IshimotoKiko. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
